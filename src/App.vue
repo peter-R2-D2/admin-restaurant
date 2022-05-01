18353f484218fc57acc320fc56c27a8cc6cf4cb0
Arturo Farias <pedro.farias.sosa@gmail.com>
2022-04-30 22:53:20 -0500

    init proyect

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+  <div class="bg-indigo-700 h-screen">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pt-8">
+    <h1 class="text-white text-center text-3xl">Hola desde home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeAdmin'
+
+}
+</script>
+
+<style>
+</style>
